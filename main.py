c503b9f86187ecd45ef8d9e4d1a5e6cb9041c9cb
John Byrne <johnbyrnejb@gmail.com>
2023-09-20 14:49:51 +0100

    Updated to look ready for first pass, initial preparation for connecting to the chatbot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Request
-from starlette.templating import Jinja2Templates
 import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
@@ -8,13 +9,12 @@ templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-# Simple chatbot response function (replace with your chatbot logic)
 def chatbot_response(user_message):
     return f"Chatbot says: You said '{user_message}'"
 
 
-@app.get("/")
-def read_root(request: Request):
+@app.get("/", response_class=HTMLResponse)
+async def chatbot_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
@@ -28,5 +28,4 @@ if __name__ == "__main__":
     host = "0.0.0.0"
     port = 8000
     reload = True
-
     uvicorn.run("main:app", host=host, port=port, reload=reload)
